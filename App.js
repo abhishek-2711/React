42d5fb409e3ce1b7a3fb59122d1e370120da39e6
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2023-12-02 14:33:46 +0530

    Episode_02

--- App.js
+++ App.js
@@ -1,17 +1,15 @@
-// how we can create h1 tag in react.
-// below code will return you an object.
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "temp" }, // attributes
   "Abhishek Rathod"
 );
 console.log(heading);
-// React.createElement gives us react element not html element , it's an object.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// render method is convert the object into h1 element.
-const children = React.createElement("div", { id: "child" }, "children");
+const children = React.createElement("div", { id: "child" }, "Abhishek");
 const children2 = React.createElement("div", { id: "child2" }, "children2");
 const parent = React.createElement("div", { id: "parent" }, [
   children,
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
-# Learning React
+# Learning React 🚀🚀
+
+
+# parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement 
+- File Watching Algorithm written c++ 
+- Parcel also use caching. faster build 
+- there is a folder named parcel-cache. 
+- Bundling
+- Compress
+- Consistent Hashing 
+- Code Spliting 
+- Differential Bundling - support older browsers 
+- Diagnostic
+- Error Handling
+- Remove unneccessary code - Tree Shacking 
+- Different Dev and Production build 
