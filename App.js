d6c9daabf3b46d2394b5deb15c414709091c92f5
Abhishek Rathod <rathod.abhishek2021@gmail.com>
2024-05-26 15:01:04 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "xyz" },
-  "Hello world from React!"
-);
+// React element is not html element
 
-console.log(heading);
+// This is how we can create functional component.
+// Component -> JS Function which returns JSX.
+
+const TitleComponent = () => {
+  return <h1>This is React Title Component</h1>;
+};
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <TitleComponent />
+      <h1>This is React Functional component</h1>
+    </div>
+  );
+};
+
+const heading = React.createElement("h1", {}, "Hello React!");
+
+const jsxHeading = <h1>Hello React using jsx</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
